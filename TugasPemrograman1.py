5f148b42e8441c96647cc485001c6ba3960c9e48
Dafasr99 <anandafa.syukur@ui.ac.id>
2023-07-23 18:42:53 +0700

    progress tp1

--- TugasPemrograman1.py
+++ TugasPemrograman1.py
@@ -1,65 +1,39 @@
-class Evaluator:
-    def __init__(self):
-        self.stack = []
-        self.str = ''
+def evaluate_postfix(postfix_exp):
+    operand = []
+    operator = {'+', '-', '*', '/', '$'}
 
-    def push(self, val):
-        self.stack.append(val)
-
-    def pop(self):
-        if self.is_empty():
-            raise ValueError('Stack is empty')
-        return self.stack.pop()
-
-    def is_empty(self):
-        return len(self.stack) == 0
-
-    def calculate(self, token, first, second):
-        try:
-            if token == '+':
-                return second + first
-            elif token == '-':
-                return second - first
-            elif token == '*':
-                return second * first
-            elif token == '/':
-                try: # Handle if divided by zero
-                    self.stack.append(second // first)
-                except ZeroDivisionError as e:
-                    print("Nilai : ", second)
-                    self.str = "[Division by zero]"
-                    return
-            elif token == '$':
-                return second ** first
-        except IndexError:
-            return first
-
-    def evaluate_postfix(self, postfix_exp):
-        self.stack.clear()
-        for token in postfix_exp.split():
-            if token.isdigit():
-                self.push(int(token))
-            else:
+    for i in postfix_exp.split():
+        if i.isdigit():
+            operand.append(int(i))
+        elif i in operator:
+            if len(operand) < 2:
+                print("[Missing operand]")
+                return None
+            num2 = operand.pop()
+            num1 = operand.pop()
+            if i == '+':
+                operand.append(num1 + num2)
+            elif i == '-':
+                operand.append(num1 - num2)
+            elif i == '*':
+                operand.append(num1 * num2)
+            elif i == '/':
                 try:
-                    first = self.pop()
-                    second = self.pop()
-                    result = self.calculate(token, first, second)
-                    self.push(result)
-                except (IndexError, ValueError) as e:
-                    self.str = str(e)
-                    return
-
-        result = self.pop()
-        if not self.is_empty():
-            self.str = "[Invalid postfix expression]"
-            return
-        if self.str:
-            print("Error Messages:", self.str)
-        else:
-            print("Nilai:", result)
-
+                    operand.append(num1 // num2)
+                except ZeroDivisionError:
+                    print("[Division by zero]")
+                    operand.append(num1)
+            elif i == '$':
+                operand.append(num1 ** num2)
+    
+    if len(operand) == 1:
+        return operand[0]
+    else:
+        print("[Missing operand]")
+        return None
 
 if __name__ == '__main__':
     postfix_exp = input("Masukkan Ekspresi Postfix: ")
-    evaluator = Evaluator()
-    evaluator.evaluate_postfix(postfix_exp)
+    result = evaluate_postfix(postfix_exp)
+    if result is not None:
+        print('Result:', result)
